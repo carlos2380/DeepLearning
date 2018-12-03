002d42130018b072695c1f9089abc3c3d63257b2
Carlos <carlos.2380@hotmail.com>
2018-12-03 11:59:32 +0100

    ANN - Input layer and first hidden layer

--- Artificial_Neural_Networks/ann.py
+++ Artificial_Neural_Networks/ann.py
@@ -37,6 +37,11 @@ import keras
 from keras.models import Sequential
 from keras.layers import Dense
 
+# Initialising the ANN
+classifier = Sequential()
+
+# Adding the input layer and the first hidden layer
+classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
 
 
 # Predicting the Test set results
