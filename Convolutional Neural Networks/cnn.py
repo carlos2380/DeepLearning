84ed0dc1d37da1a1a67dbc1244cfe65dff7156f1
Carlos <carlos.2380@hotmail.com>
2018-12-03 23:49:07 +0100

    CNN - Importing Keras

---
+++ Convolutional Neural Networks/cnn.py
@@ -0,0 +1,10 @@
+# Convolutional Neural Network
+
+# Part 1 - Building the CNN
+
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
