f54bb3ba678aaf819019fff1eda1c1578440a1e6
Carlos <carlos.2380@hotmail.com>
2018-12-03 12:01:32 +0100

    ANN - Compile and fitting

--- Artificial_Neural_Networks/ann.py
+++ Artificial_Neural_Networks/ann.py
@@ -49,6 +49,11 @@ classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 're
 # Adding the output layer
 classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
 
+# Compiling the ANN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Fitting the ANN to the Training set
+classifier.fit(X_train, y_train, batch_size = 10, epochs = 100)
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
